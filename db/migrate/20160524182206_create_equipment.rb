7541a8397be69e1a2109aee3783851895fe1fecd
Sean <sjgallen@gmail.com>
2016-06-01 21:16:47 -0500

    added a schema

--- db/migrate/20160524182206_create_equipment.rb
+++ db/migrate/20160524182206_create_equipment.rb
@@ -2,7 +2,8 @@ class CreateEquipment < ActiveRecord::Migration
   def change
     create_table :equipment do |t|
       t.string :title
-      t.text :text
+      t.text :product_name
+      t.text :description
 
       t.timestamps null: false
     end
---
+++ db/migrate/20160524201503_equipment.rb
@@ -0,0 +1,4 @@
+class Equipment < ActiveRecord::Migration
+  def change
+  end
+end
